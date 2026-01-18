95a4b9499dcb5f203e722156d3a2dfc03e3b9fc4
rar12455 <rarSS@proton.me>
2026-01-18 03:38:50 +0300

    Initial commit: Added Kernel, Disk, RAM, Battery, and Uptime modules

---
+++ config.h
@@ -0,0 +1,17 @@
+/* Configuration for rarstatus */
+
+/* Update interval in seconds */
+#define INTERVAL 60
+
+/* Date format (passed to strftime) */
+//static const char *date_format = "%Y-%m-%d %H:%M";
+
+/* Formatting preferences */
+static const bool show_newline = true;
+static const bool no_newline = false;
+
+/* iso_format */
+static const bool iso_format   = true;
+
+/* Separator string */
+static const char *separator = " | ";
