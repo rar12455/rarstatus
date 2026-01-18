2b39f640188faa439ccedd8ef884777ec2a6f6e7
rar12455 <rarSS@proton.me>
2026-01-18 04:22:43 +0300

    Update config.h

--- config.h
+++ config.h
@@ -3,9 +3,6 @@
 /* Update interval in seconds */
 #define INTERVAL 60
 
-/* Date format (passed to strftime) */
-//static const char *date_format = "%Y-%m-%d %H:%M";
-
 /* Formatting preferences */
 static const bool show_newline = true;
 static const bool no_newline = false;
