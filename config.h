1a6b2c9155eec1fe41e8c9533b99e4914e3b2ffa
rar12455 <rarSS@proton.me>
2026-01-21 03:40:05 +0300

    added brightness functionality

--- config.h
+++ config.h
@@ -4,15 +4,20 @@
 #define INTERVAL 1
 
 /* Formatting preferences */
-const bool show_newline = true;
-const bool no_newline = false;
+const bool show_newline = 1;
+const bool no_newline = 0;
 
 /* iso_format */
-const bool iso_format   = true;
+const bool iso_format = 1;
 
 /* Separator string */
 const char *separator = " | ";
 
 /* disk partition mount point for disk usage look-up */
 
-char partition[30] = "/"; // by default it shows / i.e: root partition, type: df -h in terminal to see details.
+char partition[30] = "/"; // by default it shows / i.e: root partition, type: df
+                          // -h in terminal to see details.
+
+#define BRIGHTNESS_FILE_PATH "/sys/class/backlight/intel_backlight/brightness"
+#define MAX_BRIGHTNESS_FILE_PATH                                               \
+  "/sys/class/backlight/intel_backlight/max_brightness"
--- main.c
+++ main.c
@@ -187,9 +187,70 @@ void print_Hostname() {
   }
 }
 
+void brightness(int argc) {
+
+  /* modes:
+   * 0 = see brightness value
+   * 1 = see brightness percentage
+   */
+
+  if (argc == 0) {
+    FILE *file;
+    int brightness_val = 0;
+    file = fopen(BRIGHTNESS_FILE_PATH, "r");
+
+    if (!file) {
+      printf("file not found: %s\n", BRIGHTNESS_FILE_PATH);
+      return;
+    }
+
+    fscanf(file, "%d", &brightness_val); // NOLINT
+
+    printf("%d", brightness_val);
+
+    fclose(file);
+  } else {
+    FILE *file;
+    FILE *max_brightness_file;
+    float brightness_val = 0;
+    float max_brightness_val = 0;
+    float brightness_perc = 0;
+
+    // FILE 1
+
+    file = fopen(BRIGHTNESS_FILE_PATH, "r");
+
+    if (!file) {
+      printf("file not found: %s\n", BRIGHTNESS_FILE_PATH);
+      return;
+    }
+
+    fscanf(file, "%f", &brightness_val); // NOLINT
+
+    fclose(file);
+
+    // FILE 2
+
+    max_brightness_file = fopen(MAX_BRIGHTNESS_FILE_PATH, "r");
+
+    if (!max_brightness_file) {
+      printf("file not found: %s\n", MAX_BRIGHTNESS_FILE_PATH);
+      return;
+    }
+
+    fscanf(max_brightness_file, "%f", &max_brightness_val); // NOLINT
+    fclose(max_brightness_file);
+
+    brightness_perc = brightness_val / max_brightness_val * 100;
+    printf("%.0f%%", brightness_perc);
+  }
+}
+
 void main_loop() {
 
   while (1) {
+    brightness(1);
+    decorate(no_newline);
     print_Hostname();
     decorate(no_newline);
     getuseddiskinfo(partition);
@@ -203,6 +264,8 @@ void main_loop() {
     uptime();
     decorate(no_newline);
     datetime(iso_format);
+
+    putchar('\n');
 
     fflush(stdout);  // needed for print STDOUT
     sleep(INTERVAL); /// sleep value
