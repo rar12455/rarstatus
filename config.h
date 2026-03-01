fdbf0b97234afb0d6011c4c0e954c12f3f61040c
rar12455 <rarSS@proton.me>
2026-03-01 17:17:17 +0300

    modified default run_command

--- config.h
+++ config.h
@@ -7,19 +7,20 @@
 #define INTERVAL 1
 
 /* Brightness file paths */
-#define BRIGHTNESS_FILE_PATH     "/sys/class/backlight/intel_backlight/brightness"
-#define MAX_BRIGHTNESS_FILE_PATH "/sys/class/backlight/intel_backlight/max_brightness"
+#define BRIGHTNESS_FILE_PATH "/sys/class/backlight/intel_backlight/brightness"
+#define MAX_BRIGHTNESS_FILE_PATH                                               \
+        "/sys/class/backlight/intel_backlight/max_brightness"
 
 /* Battery paths */
-#define BATTERY_PATH       "/sys/class/power_supply/BAT0/capacity"
+#define BATTERY_PATH "/sys/class/power_supply/BAT0/capacity"
 #define BATTERY_STATE_PATH "/sys/class/power_supply/BAT0/status"
 
 /* cat_a_file settings */
-#define CAT_FILE_PATH   "/home/USER/foo.txt"
+#define CAT_FILE_PATH "/home/USER/foo.txt"
 #define CAT_FILE_LENGTH 64
 
 /* run_command setting */
-#define COMMAND_NAME "ls -l"
+#define COMMAND_NAME "echo 'hello world!' "
 
 /* Variables defined in config.c */
 extern const bool  iso_format;
